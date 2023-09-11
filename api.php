7e81090c72808dca2ef0c7938f104b3918d8023a
success2202 <successceejay@gmail.com>
2023-09-12 00:18:56 +0100

    api

---
+++ api.php
@@ -0,0 +1,73 @@
+<?php
+//include database
+require_once('db_connect.php');
+// Set the response headers to indicate JSON content
+header('Content-Type: application/json');
+
+//get  people person by name
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    if (isset($_GET['person'])) {
+        $name = $_GET['person'];
+        $sql = "SELECT * FROM users WHERE name = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $name);
+        $stmt->execute();
+        $result = $stmt->get_result();
+        $persons = [];
+        while ($row = $result->fetch_assoc()) {
+            $persons[] = $row;
+        }
+        echo json_encode($persons);
+    } else {
+        $sql = "SELECT * FROM users";
+        $result = $conn->query($sql);
+        $persons = [];
+        while ($row = $result->fetch_assoc()) {
+            $persons[] = $row;
+        }
+        echo json_encode($persons, JSON_PRETTY_PRINT);
+    }
+    // Post method to Insert data into the database
+} elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        global $conn;
+        $data = json_decode(file_get_contents('php://input'), true);
+        $name = $data['name'];
+        
+        // Insert data into the database
+        $sql = "INSERT INTO users (name) VALUES (?)";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('s', $name);
+    
+    if ($stmt->execute()) {
+        echo json_encode(['message' => 'Person created successfully']);
+    } else {
+        echo json_encode(['message' => 'Failed to create person']);
+    }
+    //editing/updating person using ID
+}elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+        $data = json_decode(file_get_contents('php://input'), true);
+        $id = $data['id'];
+        $name = $data['name'];
+        $sql = "UPDATE users SET name = ? WHERE id = ?";
+        $stmt = $conn->prepare($sq);
+        $stmt->bind_param('si', $name, $id);
+    if ($stmt->execute()) {
+        echo json_encode(['message' => 'Person updated successfully']);
+    } else {
+        echo json_encode(['message' => 'Failed to update person']);
+    }
+// deleteing person by ID
+}elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+        parse_str(file_get_contents("php://input"), $data);
+        $id = $data['id'];
+        $sql = "DELETE FROM users WHERE id = ?";
+        $stmt = $conn->prepare($sql);
+        $stmt->bind_param('i', $id);
+if ($stmt->execute()) {
+    echo json_encode(['message' => 'Person deleted successfully']);
+} else {
+    echo json_encode(['message' => 'Failed to delete person']);
+}
+
+}
+?>
